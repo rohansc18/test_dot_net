78d7315ce8ca790603345fecd8da1d0d9c271d10
abhi <abhijeet@maventra.in>
2025-05-04 12:35:05 +0530

    new pro

---
+++ Crypto-Box/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Crypto_Box.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Crypto-Box/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace Crypto_Box.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
